83e4c8d6a4bce10cefddbc5a76ac7ca6705a3565
juanmiguel431 <juanmiguel431@gmail.com>
2023-08-19 16:55:26 -0400

    Cleaning default template.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-// import './todo-list';
-import './store-test';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>JMPC</h1>
     </div>
   );
 }
