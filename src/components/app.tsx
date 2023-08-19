dba660bf431a96bf3c24275986b5c37561409dc4
juanmiguel431 <juanmiguel431@gmail.com>
2023-08-19 18:15:36 -0400

    Using observer, Observer, useObserver with mobx-react

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -4,8 +4,8 @@ import Test from './test';
 function App() {
   return (
     <div className="app">
-      <h1>JMPC</h1>
-      <Test />
+      <h1>My React App</h1>
+      <Test title="Using MobX" />
     </div>
   );
 }
--- src/components/test.tsx
+++ src/components/test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import RootStore from '../stores/root-store';
-import { Observer } from 'mobx-react';
+import { observer, Observer, useObserver } from 'mobx-react';
 
 const store = new RootStore();
 const userId = store.data.user.add('Juan Miguel');
@@ -8,21 +8,89 @@ const todoId = store.data.todo.add(userId, 'Task 1');
 
 const todo = store.data.todo.getById(todoId);
 
-export const Test: React.FC = () => {
+type TestProps = {
+  title: string;
+}
+
+class TestClassBase extends React.Component<TestProps> {
+
+  render() {
+    console.log('Render');
+    return (
+      <>
+        <h1>{this.props.title}</h1>
+        <h2>
+          <Observer>
+            {() => {
+              console.log('JMPC1');
+              return <>{todo?.name}</>;
+            }}
+          </Observer>
+        </h2>
+        <button onClick={_ => todo?.updateName('Do the homework')}>Do the homework</button>
+        <button onClick={_ => todo?.updateName('Watch television')}>Watch television</button>
+      </>
+    );
+  }
+}
+
+const TestClassBase2 = observer(class TestClassBase2 extends React.Component<TestProps> {
+
+  render() {
+    console.log('Render');
+    return (
+      <>
+        <h1>{this.props.title}</h1>
+        <h2>{todo?.name}</h2>
+        <button onClick={_ => todo?.updateName('Do the homework')}>Do the homework</button>
+        <button onClick={_ => todo?.updateName('Watch television')}>Watch television</button>
+      </>
+    );
+  }
+})
+
+//Observer
+const Test: React.FC<TestProps> = (props) => {
   return (
     <>
-      <h1>
+      <h1>{props.title}</h1>
+      <h2>
         <Observer>
           {() => {
             console.log('JMPC1');
             return <>{todo?.name}</>;
           }}
         </Observer>
-      </h1>
+      </h2>
       <button onClick={_ => todo?.updateName('Do the homework')}>Do the homework</button>
       <button onClick={_ => todo?.updateName('Watch television')}>Watch television</button>
     </>
   );
 }
 
-export default Test;
+//useObserver  -- This is deprecated.
+const Test2: React.FC<TestProps> = (props) => {
+  return useObserver(() => (
+    <>
+      <h1>{props.title}</h1>
+      <h2>{todo?.name}</h2>
+      <button onClick={_ => todo?.updateName('Do the homework')}>Do the homework</button>
+      <button onClick={_ => todo?.updateName('Watch television')}>Watch television</button>
+    </>
+  ));
+}
+
+const Test3: React.FC<TestProps> = observer(function Test3(props) {
+  return (
+    <>
+      <h1>{props.title}</h1>
+      <h2>{todo?.name}</h2>
+      <button onClick={_ => todo?.updateName('Do the homework')}>Do the homework</button>
+      <button onClick={_ => todo?.updateName('Watch television')}>Watch television</button>
+    </>
+  );
+})
+
+export default Test3;
+// export default observer(TestClassBase2);
+// export default TestClassBase2;
